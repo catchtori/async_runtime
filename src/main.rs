b843d952c34251127109e5e3c0e04a3c5fae583c
栗威 <3210106175@zju.edu.cn>
2023-09-12 00:16:58 +0800

    complete

--- src/main.rs
+++ src/main.rs
@@ -48,3 +48,121 @@ fn block_on<F:Future>(future: F) -> F::Output {
         }
     }
 }
+
+strcut Signal {
+    state: Mutex<State>,
+    cond: Condvar,
+}
+
+enum State {
+    Empty,
+    Waiting,
+}
+
+fn wait(&self) {
+    let mut state: MutexGuard<'_, State> = self.state.lock().unwrap();
+    match *state {
+        State::Notified => *state = State::Empty,
+        State::Waiting => {
+            panic!("wait");
+        }
+        State::Empty => {
+            *state = State::Waiting;
+            while let State::Waiting = *state {
+                state = self.cond.wait(state).unwrap();
+            }
+        }
+    }
+}
+
+fn notify(&self) {
+    let mut state: MutexGuard<'_, State> = self.state.lock().unwrap();
+    match *state {
+        State::Notified => {}
+        State::Waiting => {
+            *state = State::Empty;
+            self.cond.notify_one();
+        }
+        State::Empty => *state = State::Notified,
+    }
+}
+
+impl Wake for Signal {
+    fn wake(self: Arc<Self>) {
+        self.notify();
+    }
+}
+
+#[stable(feature = "wake_trait", since = "1.51.0")]
+impl<W: Wake + Send + Sync + 'static> From<Arc<W>> for Waker {
+    fn from(waker: Arc<W>) -> Waker {
+        unsafe { Waker::from_raw(waker: raw_waker(waker)) }
+    }
+}
+
+fn main() {
+    block_on(future: Demo);
+}
+
+fn block_on<F:Future>(future: F) -> F::Output {
+    let mut fut: Pin<&mut F> = std::pin::pin!(future);
+    let signal: Arc<Signal> = Arc::new(data: Siganl::new());
+    let waker: Waker = Waker::from(siganl.clone());
+
+    let mut cx: Context<'_> = Context::from_waker(&waker);
+
+    loop {
+        if let Poll::Ready(output: <F as Future>::Output) = fut.as_mut().poll(&mut cx) {
+            return output
+        }
+        signal.wait();
+    }
+}
+
+struct Task {
+    future: RefCell<BoxFuture<'static, ()>>,
+    siganl: Arc<Signal>,
+}
+unsafe impl Send for Task {}
+unsafe impl Sync for Task {}
+
+impl Wake for Task {
+    fn wake(self: Arc<Self>) {
+        RUNNABLE.with(|runnable: &Mute<VecDeque<Arc<Task>>>>| runnable.lock().unwrap().push_back(Arc::clone()); 
+        self.signal.notify();
+    }
+}
+
+loop {
+    if let Poll::Ready(output: <F as Future>::Output) = main_fut.as_mut().poll(&mut cx) {
+        return output;
+    }
+    while let Some(task: Arc<Task>) = runnable.lock().unwrap().pop_front()) {
+        let waker: Waker = Waker::from(task.clone());
+        let mut cx: Context<'_> = Context::from_waker(&waker);
+        let _ = task.future.borrow_mut().as_mut().poll(&mut cx);
+    }
+    signal.wait();
+}
+
+scoped_thread_local! {static SIGNAL: Arc<Signal>}
+scoped_thread_local! {static RUNNABLE: Mute<VecDeque<Arc<Task>>>}
+
+let runnable: Mute<VecDeque<Arc<Task>>> = Mutex::new(VecDeque::with_capacity(1024));
+
+SIGNAL.set(t: &signal, f: || {
+    RUNNABLE.set(t: &runnable, f: || {
+        loop {
+            if let poll::Ready(output: <F as Future>::Output) = main_fut.as_mut().poll(&mut cx) {
+                return output
+            }
+            while let Some(task: Arc<Task>) = runnable.lock().unwrap().pop_front()) {
+                let waker: Waker = Waker::from(task.clone());
+                let mut cx: Context<'_> = Context::from_waker(&waker);
+                let _ = task.future.borrow_mut().as_mut().poll(&mut cx);
+            }
+            signal.wait();
+        }
+    }); 
+});
+
